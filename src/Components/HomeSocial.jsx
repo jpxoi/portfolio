b0675dadded08e25705ad0b8884321fefaa41120
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-27 17:41:13 +0000

    Update social links with aria-labels

--- src/Components/HomeSocial.jsx
+++ src/Components/HomeSocial.jsx
@@ -3,9 +3,9 @@ import SocialLink from "../Atoms/SocialLink";
 function HomeSocial() {
     return (
         <div className="home__social">
-            <SocialLink route="https://linkedin.com/in/jpxoi" icon="bx bxl-linkedin-square" alt="linkedin social button" />
-            <SocialLink route="https://github.com/jpxoi" icon="bx bxl-github" alt="github social button" />
-            <SocialLink route="https://instagram.com/jpxoi" icon="bx bxl-instagram" alt="instagram social button" />
+            <SocialLink route="https://linkedin.com/in/jpxoi" icon="bx bxl-linkedin-square" alt="linkedin social button" aria-label="LinkedIn" />
+            <SocialLink route="https://github.com/jpxoi" icon="bx bxl-github" alt="github social button" aria-label="GitHub" />
+            <SocialLink route="https://instagram.com/jpxoi" icon="bx bxl-instagram" alt="instagram social button" aria-label="Instagram" />
         </div>
     )
 }
