8393f44e50cdc42b6837e86fb3de32c544fc2d22
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-27 00:00:36 +0000

    Update ServicesModal component layout

--- src/Components/ServicesModal.jsx
+++ src/Components/ServicesModal.jsx
@@ -4,22 +4,24 @@ function ServicesModal(props) {
 
     return (
         <div className="services__modal">
-            <div className="services__modal-content">
+            <div className="services__modal-content max-h-[90dvh]">
                 <i className="bx bx-x services__modal-close"></i>
+                <div className="max-h-[75dvh] overflow-scroll">
+                    <h3 className="services__modal-title">{ props.title }</h3>
 
-                <h3 className="services__modal-title">{ props.title }</h3>
+                    <p className="services__modal-description text-pretty">{ props.description }</p>
 
-                <p className="services__modal-description">{ props.description }</p>
-
-                <ul className="services__modal-list">
-                    {props.items.map((item, index) => (
-                        <ServicesModalItem
-                            key={index}
-                            title={item.title}
-                            description={item.description}
-                        />
-                    ))}
-                </ul>
+                    <ul className="services__modal-list">
+                        {props.items.map((item, index) => (
+                            <ServicesModalItem
+                                key={index}
+                                title={item.title}
+                                description={item.description}
+                            />
+                        ))}
+                    </ul>
+                </div>
+                
             </div>
         </div>
     )
