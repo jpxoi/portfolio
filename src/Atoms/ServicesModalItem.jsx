dcaea2f55335c2ea5b4302fe54f2bb16ed8e8a4f
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-27 00:00:25 +0000

    Update ServicesModalItem component to add text-pretty class

--- src/Atoms/ServicesModalItem.jsx
+++ src/Atoms/ServicesModalItem.jsx
@@ -2,7 +2,7 @@ function ServicesModalItem( props ) {
     return (
         <li className="services__modal-item">
             <i className="bx bx-check services__modal-icon"></i>
-            <p className="services__modal-info">
+            <p className="services__modal-info text-pretty">
                 <b>{ props.title }:</b> { props.description }
             </p>
         </li>
