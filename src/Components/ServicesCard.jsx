78972e75ff5bfaff94b06951b5016b34fe0257c8
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-27 00:00:29 +0000

    Update services card title style

--- src/Components/ServicesCard.jsx
+++ src/Components/ServicesCard.jsx
@@ -3,7 +3,7 @@ import ServicesModal from "./ServicesModal"
 function ServicesCard(props) {
     return (
         <div className="services__card">
-            <h3 className="services__title">{ props.title }</h3>
+            <h3 className="services__title text-balance">{ props.title }</h3>
 
             <span className="services__button">
                 See More <i className="bx bx-right-arrow-alt services__icon"></i>
