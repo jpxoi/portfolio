2e199d21c3b6b89bc0b1512cf6f0b7dbd35724b4
Jean Paul Fernandez <jpxoi@icloud.com>
2024-06-05 07:33:47 +0100

    chore: Add hidden text for accessibility in social links

--- src/Atoms/FootSocialLink.jsx
+++ src/Atoms/FootSocialLink.jsx
@@ -3,6 +3,7 @@ function FootSocialLink({ link, icon, alt }) {
         <li>
             <a href={ link } target="_blank" rel="noreferrer" className="footer__social-link" alt={ alt } aria-label={ alt }>
                 <i className={ icon }></i>
+                <span className="hidden">{ alt }</span>
             </a>
         </li>
         
