29af9c227933032a7192734a0c9c4357f1c69e99
Jean Paul Fernandez <jpxoi@icloud.com>
2024-09-11 15:21:11 -0500

    Remove unused SpeedInsights component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { useEffect } from 'react'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import Landing from './Pages/Landing'
 import Error404 from './Pages/Error404';
 import Privacy from './Pages/Privacy';
@@ -75,7 +74,6 @@ function App() {
   return (
     <>
       <Analytics/>
-      <SpeedInsights/>
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
